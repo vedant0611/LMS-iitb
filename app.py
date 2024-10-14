0293b6af069776f18389c78cad317aeaafe86e85
vedant0611 <tadla.vedant@gmail.com>
2024-10-14 21:10:01 +0530

    Trial

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from modules.auth.routes import router as auth_router
+from modules.librarian.routes import router as librarian_router
+from modules.member.routes import router as member_router
+
+app = FastAPI(
+    title = "Library Management System"
+)
+
+app.include_router(auth_router)
+app.include_router(librarian_router)
+app.include_router(member_router)
